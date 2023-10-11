955ebe6a97795f3dd58e489f759ce33bb73a873a
xubinz <xubinz@student.unimelb.edu.au>
2023-10-11 18:37:37 +1100

    edit the path in cleaned data R script

--- Tableau/data/mga2020_55/esrishape/customised_delivery/MELB_METRO_VAR1-0/PTV/CleanTransportData.R
+++ Tableau/data/mga2020_55/esrishape/customised_delivery/MELB_METRO_VAR1-0/PTV/CleanTransportData.R
@@ -4,7 +4,7 @@ library(sf)
 library(tidyverse)
 
 # read the shp file and transfer to dataframe
-data_shp <- st_read("/Users/KOBE/Desktop/ALL/master/Information Visualizaion/ASS3/Melbourne-City-Travel-Guide/Tableau/data/Order_21E6VV/mga2020_55/esrishape/customised_delivery/MELB_METRO_VAR1-0/PTV/PTV_METRO_TRAM_STOP.shp")
+data_shp <- st_read("PTV_METRO_TRAM_STOP.shp")
 data <- as.data.frame(data_shp)
 
 # clean the dataset by seperating the 
@@ -12,4 +12,4 @@ tidy_data <- data %>%
   separate_rows(ROUTEUSSP, sep = ",", convert = TRUE)
 head(tidy_data)
 
-st_write(st_as_sf(tidy_data), "/Users/KOBE/Desktop/ALL/master/Information Visualizaion/ASS3/Melbourne-City-Travel-Guide/Tableau/data/Order_21E6VV/mga2020_55/esrishape/customised_delivery/MELB_METRO_VAR1-0/PTV/CLEANED_PTV_METRO_TRAM_STOP.shp")
+st_write(st_as_sf(tidy_data), "CLEANED_PTV_METRO_TRAM_STOP.shp")
