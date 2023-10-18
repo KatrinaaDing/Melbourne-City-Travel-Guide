ab7804e99a0ef40f9a42535ce122fe93488d0410
xubinz <xubinz@student.unimelb.edu.au>
2023-10-18 15:38:18 +1100

    Finished the tableau map

--- Tableau/Transport/data/cleanPedestrainData.py
+++ Tableau/Transport/data/cleanPedestrainData.py
@@ -7,7 +7,7 @@ sensor_geoData = gpd.read_file('data/pedestrian-counting-system-sensor-locations
 tramStop_data = gpd.read_file(
     'data/mga2020_55/esrishape/customised_delivery/MELB_METRO_VAR1-0/PTV/CLEANED_PTV_METRO_TRAM_STOP.shp')
 
-
+print(len(tramStop_data))
 # clean the sensor dataset
 sensors_geoData_cleaned = sensor_geoData.drop(['installation_date', 'note','location_type',
                         'status', 'direction_1', 'direction_2','sensor_name'], axis=1)
@@ -19,23 +19,26 @@ tramStop_data = tramStop_data.to_crs("EPSG:28355")
 def find_nearest_sensor(tramStop_data, sensors_geoData_cleaned):
     # 计算到所有传感器的距离
     distances = sensors_geoData_cleaned.geometry.distance(tramStop_data.geometry)
-    closest_sensor_index = distances.idxmin()
+    limitedRange = distances[distances <= 1000]
+    if limitedRange.empty:
+        return None
+    
+    closest_sensor_index = limitedRange.idxmin()
     
     closest_sensor_id = sensors_geoData_cleaned.loc[closest_sensor_index, 'location_id']
     return closest_sensor_id
 
-tramStop_data['location_id'] = None  # 这会初始化一个空列
-
-# 对于每个站点，获取最近的传感器
+# put the closest sensor location id into the data
+tramStop_data['location_id'] = None
 tramStop_data['location_id'] = tramStop_data.apply(find_nearest_sensor, args=(sensors_geoData_cleaned,), axis=1)
 
-tramStop_data_merged_SensorLocation = pd.merge(tramStop_data, sensors_geoData_cleaned, on="location_id")
+tramStop_data_merged_SensorLocation = pd.merge(tramStop_data, sensors_geoData_cleaned, on="location_id", how='left')
 tramStop_data_merged_SensorLocation.rename(columns={'latitude': 'sensor_latitude', 
                                                     'longitude': 'sensor_longitude',
                                                     'geometry_y': 'sensor_geometry',
                                                     'sensor_description': 'Sensor Name'}, inplace=True)
 
-print(tramStop_data_merged_SensorLocation.columns)
+print(tramStop_data_merged_SensorLocation)
 
 
 # clean the pedestrain data type
@@ -47,19 +50,25 @@ pedestrain_Data['Hour'] = pedestrain_Data['Hour'].astype(int)
 geo_columns = pedestrain_Data.columns[2:]
 pedestrain_Data[geo_columns] = pedestrain_Data[geo_columns].astype(float)
 
-
+# To calculate the mean pedestrain count in this month by hour 
 average_pedestrian_per_Hour = pedestrain_Data.groupby('Hour')[geo_columns].mean()
 average_pedestrian_per_Hour = average_pedestrian_per_Hour.T
-average_pedestrian_per_Hour.index.name = "Sensor Name"
 
-average_pedestrian_per_Hour_withLocation = pd.merge(tramStop_data_merged_SensorLocation, average_pedestrian_per_Hour, on='Sensor Name')
-                                                                               
+# To put the Hour as one column and value as another column
+average_pedestrian_per_Hour_reset = average_pedestrian_per_Hour.reset_index()
+average_pedestrian_per_Hour_melt = pd.melt(average_pedestrian_per_Hour_reset, id_vars='index', var_name='Hour', value_name='Value')
+
+# To let the sensor name as the index of dataset
+average_pedestrian_per_Hour_melt = average_pedestrian_per_Hour_melt.rename(
+    columns={'index': 'Sensor Name'})
+average_pedestrian_per_Hour_withLocation = pd.merge(
+    tramStop_data_merged_SensorLocation, average_pedestrian_per_Hour_melt, on='Sensor Name', how='left')
+
 average_pedestrian_per_Hour_withLocation["geometry_x"].to_crs("EPSG:4326")
 average_pedestrian_per_Hour_withLocation["sensor_geometry"].to_crs("EPSG:4326")
 
-# # 4. 结果输出
-# # 输出月平均人流量数据到一个新的csv文件
-# #
+
+# Output the dataset as csv file
 average_pedestrian_per_Hour_withLocation.to_csv('data/average_pedestrian_per_Hour_WithLocation.csv')
 
 
