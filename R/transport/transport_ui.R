476dfea38b88e04b23fa82a988f9e232092db14c
xubinz <xubinz@student.unimelb.edu.au>
2023-10-19 16:21:54 +1100

    Add the map and chart into R script

--- R/transport/transport_ui.R
+++ R/transport/transport_ui.R
@@ -1,10 +1,30 @@
+tramStop_Pedestrian_Map_id <- 'tramStop_Pedestrian_Map'
+pedestrian_Count_per_Hour <- 'pedestrian_Count_per_Hour'
 transport_tab <- tabItem(
   tabName = "transport",
   h4("Transport"),
-  #fluidRow(
-  #  box(width = 6, status = 'primary',
-  #      height = chart_box_height + 20 
-  #      )
-  #)
+  column(
+    width = 8,
+    box(width = 12, 
+           status = 'primary',
+           height = chart_box_height + 20, 
+           tableauPublicViz(id = tramStop_Pedestrian_Map_id,
+                            url = "https://public.tableau.com/views/TramMapwithPedestrianCount/Tramandsensormap?:language=zh-CN&publish=yes&:display_count=n&:origin=viz_share_link",
+                            height=paste0(chart_box_height + 200, "px")
+                        )
+        )
+  ),
+  column(
+    width = 4,
+    box(
+      width = 12,
+      status = 'primary',
+      height = chart_box_height,
+      tableauPublicViz(id = pedestrian_Count_per_Hour,
+                       url = "https://public.tableau.com/views/PedestianCountbyHours/EachSensorBarChart?:language=zh-CN&:display_count=n&:origin=viz_share_link",
+                       height=paste0(chart_box_height, "px"),
+                       
+      )
+    )
+  )
 )
-
--- Tableau/Transport/data/mga2020_55/esrishape/customised_delivery/MELB_METRO_VAR1-0/PTV/CleanTransportData.R
+++ Tableau/Transport/data/mga2020_55/esrishape/customised_delivery/MELB_METRO_VAR1-0/PTV/CleanTransportData.R
@@ -4,9 +4,8 @@ library(sf)
 library(tidyverse)
 
 # read the shp file and transfer to dataframe
-data_shp <- st_read("PTV_METRO_TRAM_STOP.shp")
+data_shp <- st_read("./PTV_METRO_TRAM_STOP.shp")
 data <- as.data.frame(data_shp)
-
 # clean the dataset by seperating the 
 tidy_data <- data %>%
   separate_rows(ROUTEUSSP, sep = ",", convert = TRUE)
