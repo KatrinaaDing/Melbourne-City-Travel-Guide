964a579f8efac14754cdcef91020cad9455eb607
xubinz <xubinz@student.unimelb.edu.au>
2023-10-19 17:02:52 +1100

    working on tableau interaction in shiny

--- R/transport/transport_server.R
+++ R/transport/transport_server.R
@@ -1,3 +1,6 @@
 transportServer <- function(input, output, session) {
-
+    observeEvent(input$tramStop_Pedestrian_Map_id_mark_selection_changed, {
+        print("Test here")
+        print(input$tramStop_Pedestrian_Map_id_mark_selection_changed)
+    })
 }
